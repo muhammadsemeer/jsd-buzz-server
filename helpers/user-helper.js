6df965734bc65dda643a0e4686f886e7ba01db09
muhammadsemeer <muhammadsemeerali@gmail.com>
2021-04-07 17:48:46 +0530

    added user get route and function

--- helpers/user-helper.js
+++ helpers/user-helper.js
@@ -1,4 +1,15 @@
 const db = require("../config/connection"),
   { USER } = require("../config/collection");
 
-module.exports = {};
+module.exports = {
+  get: () => {
+    return new Promise(async (resolve, reject) => {
+      try {
+        let users = db.get().collection(USER).find({}).toArray();
+        resolve(users);
+      } catch (error) {
+        reject(error);
+      }
+    });
+  },
+};
--- routers/user.js
+++ routers/user.js
@@ -1,3 +1,15 @@
-const router = require("express").Router();
+const router = require("express").Router(),
+  userHelpers = require("../helpers/user-helper");
+
+router.get("/", (req, res) => {
+  userHelpers
+    .get()
+    .then((users) => {
+      res.json(users);
+    })
+    .catch((error) => {
+      res.sendStatus(500);
+    });
+});
 
 module.exports = router;
